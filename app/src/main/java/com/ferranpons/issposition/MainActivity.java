cbcec56f2a1a3841b644dc99cbea6e2b5a144693
Ferran Pons <ferran.pons@scmspain.com>
2017-05-26 09:26:27 +0200

    Removed unused import

--- app/src/main/java/com/ferranpons/issposition/MainActivity.java
+++ app/src/main/java/com/ferranpons/issposition/MainActivity.java
@@ -18,7 +18,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;

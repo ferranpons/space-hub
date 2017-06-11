74fd5be09c52c4fe93f227dccd7ce721a269b680
Ferran Pons <ferran.pons@scmspain.com>
2017-06-11 22:29:19 +0200

    Added Schedule Presenter. Added first Test

--- app/src/test/java/com/ferranpons/issposition/schedule/SchedulePresenterShould.kt
+++ app/src/test/java/com/ferranpons/issposition/schedule/SchedulePresenterShould.kt
@@ -1,14 +1,22 @@
 package com.ferranpons.issposition.schedule
 
-import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 
 class SchedulePresenterShould {
 
+    val scheduleView: ScheduleViewInterface = mock(ScheduleViewInterface::class.java)
+
     @Test
-    fun firstTests() {
-        assertTrue(true)
+    fun retrieveThePassTimesListWhenLocationProvided() {
+        val latitude = 43.02
+        val longitude = 23.45
+        val schedulePresenter = SchedulePresenter(scheduleView, PassTimesUseCase())
+        schedulePresenter.getPassTimesFor(latitude, longitude)
+
+        verify(scheduleView).showPassTimes(emptyList())
     }
 
 }

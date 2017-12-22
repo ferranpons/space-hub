683abeb4f76ea4ffd0a08a4ab41ddff6626cd572
Ferran Pons <ferran.pons@scmspain.com>
2017-12-22 13:05:46 +0100

    Changed compile to implementation. Added bottom bar library

--- app/src/main/java/com/ferranpons/issposition/settings/SettingsFragment.kt
+++ app/src/main/java/com/ferranpons/issposition/settings/SettingsFragment.kt
@@ -15,7 +15,7 @@ class SettingsFragment: Fragment() {
         super.onAttach(context)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_about, container, false)
         return view
     }

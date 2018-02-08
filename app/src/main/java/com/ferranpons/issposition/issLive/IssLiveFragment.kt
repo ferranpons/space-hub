d0e26c46647403a1ea0f25259919e2ae193c60e4
Ferran Pons <ferranpons@gmail.com>
2018-02-08 12:09:47 +0100

    Added Bottom Bar and fragment layouts

--- app/src/main/java/com/ferranpons/issposition/issLive/IssLiveFragment.kt
+++ app/src/main/java/com/ferranpons/issposition/issLive/IssLiveFragment.kt
@@ -16,7 +16,7 @@ class IssLiveFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_iss_live, container, false)
         return view
     }
 }
--- app/src/main/java/com/ferranpons/issposition/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/ferranpons/issposition/schedule/ScheduleFragment.kt
@@ -16,7 +16,7 @@ class ScheduleFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_schedule, container, false)
         return view
     }
 }

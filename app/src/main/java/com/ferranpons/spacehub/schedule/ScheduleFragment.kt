5dc550fc5ce99a99af87ade639bff402d75f7362
Ferran Pons <ferran.pons@scmspain.com>
2018-04-11 18:38:06 +0200

    Added schedule content and adapter and recycler view

--- app/src/main/java/com/ferranpons/spacehub/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/ferranpons/spacehub/schedule/ScheduleFragment.kt
@@ -3,19 +3,19 @@ package com.ferranpons.spacehub.schedule
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ferranpons.spacehub.R
-import com.ferranpons.spacehub.issTracking.IssTrackingApi
-import com.ferranpons.spacehub.issTracking.IssTrackingApiInterface
-import com.ferranpons.spacehub.issTracking.IssTrackingPresenterInterface
-import com.ferranpons.spacehub.issTracking.IssTrackingViewInterface
-import com.ferranpons.spacehub.issTracking.IssTrackingPresenter
-import com.ferranpons.spacehub.issTracking.IssTrackingInteractor
+import com.ferranpons.spacehub.issTracking.*
+import com.ferranpons.spacehub.passTimes.PassTimesAdapter
 
 class ScheduleFragment : Fragment(), IssTrackingViewInterface {
     private lateinit var issTrackingPresenter: IssTrackingPresenterInterface
+    private lateinit var recyclerView: RecyclerView
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -29,6 +29,16 @@ class ScheduleFragment : Fragment(), IssTrackingViewInterface {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(view.context)
+        recyclerView = view.findViewById(R.id.schedule_recyclerview)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        recyclerView.itemAnimator = DefaultItemAnimator()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         issTrackingPresenter.retrieveCurrentPosition()
@@ -39,6 +49,7 @@ class ScheduleFragment : Fragment(), IssTrackingViewInterface {
     }
 
     override fun setIssPosition(position: IssTrackingApiInterface.IssPosition?) {
+        issTrackingPresenter.retrievePassTimes(position?.latitude!!, position.longitude)
     }
 
     override fun willRetrievePassTimes() {
@@ -60,6 +71,10 @@ class ScheduleFragment : Fragment(), IssTrackingViewInterface {
     }
 
     override fun showPassTimes(passTimes: MutableList<IssTrackingApiInterface.PassTime>?) {
+        val scheduleAdapter = PassTimesAdapter(passTimes!!)
+
+        recyclerView.adapter = scheduleAdapter
+        scheduleAdapter.notifyDataSetChanged()
     }
 
     override fun showPeopleInSpace(people: MutableList<IssTrackingApiInterface.Person>?) {

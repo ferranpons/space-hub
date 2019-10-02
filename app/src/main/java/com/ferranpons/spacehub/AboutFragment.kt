7df243ef2bdc2b18a4d8754355c66aa36e9443fc
Ferran Pons <ferranpons@gmail.com>
2019-10-02 09:36:27 +0200

    Upgrade project to the latest SDK version

--- app/src/main/java/com/ferranpons/spacehub/AboutFragment.kt
+++ app/src/main/java/com/ferranpons/spacehub/AboutFragment.kt
@@ -14,7 +14,7 @@ class AboutFragment : DialogFragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.fragment_about, container)
+        val view = inflater.inflate(R.layout.fragment_about, container)
         /*val projectUrl = view.findViewById(R.id.projectUrl) as TextView
         projectUrl.setOnClickListener { view1 ->
             if (activity != null) {
